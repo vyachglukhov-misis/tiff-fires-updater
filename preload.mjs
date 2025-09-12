7378b9645c7734807eadb6f5a3b41abc2be76e06
Viacheslav Glukhov <m2210359@edu.misis.ru>
2025-09-12 15:43:00 +0300

    feat: init

---
+++ preload.mjs
@@ -0,0 +1,12 @@
+// preload.mjs
+import dotenv from "dotenv"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Загружаем .env
+dotenv.config({ path: path.join(__dirname, ".env") })
+
+console.log("✅ .env variables loaded")
