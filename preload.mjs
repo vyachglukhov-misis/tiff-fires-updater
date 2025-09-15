37fe660dfabed4c49c0c9a0f4705f7b419167689
Viacheslav Glukhov <m2210359@edu.misis.ru>
2025-09-15 17:06:18 +0300

    lol

--- preload.mjs
+++ preload.mjs
@@ -3,8 +3,8 @@ import dotenv from "dotenv"
 import path from "path"
 import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Загружаем .env
 dotenv.config({ path: path.join(__dirname, ".env") })
